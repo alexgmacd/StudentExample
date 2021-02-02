eb0c252d3206824e9ebd72320abde9276c8c2643
alexgmacd <78035554+alexgmacd@users.noreply.github.com>
2021-02-02 11:24:04 -0500

    Commit #5

--- src/ca/sheridancollege/week1/softwarefundamentals/partTimeStudent.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/partTimeStudent.java
@@ -1,5 +1,6 @@
 /*
 Adding code on PartTimeStudent branch - Commit #4
+Adding comment - Commit #5
 */
 package ca.sheridancollege.week1.softwarefundamentals;
 
@@ -8,5 +9,5 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * @author Alex
  */
 public class partTimeStudent extends Student  {
-    
+   
 }
