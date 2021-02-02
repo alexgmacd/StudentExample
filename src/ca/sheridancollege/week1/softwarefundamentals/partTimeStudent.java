318d3c4b14f69091916c87d55f5f7459ae361be3
Alex <Alex@192.168.2.31>
2021-02-02 11:14:48 -0500

    Commit #4 Testing Branches

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/partTimeStudent.java
@@ -0,0 +1,12 @@
+/*
+Adding code on PartTimeStudent branch - Commit #4
+*/
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author Alex
+ */
+public class partTimeStudent extends Student  {
+    
+}
