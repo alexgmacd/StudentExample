4e39c279b6fc5c7e804ca2d0a88044bd2b9763cb
alexgmacd <78035554+alexgmacd@users.noreply.github.com>
2021-02-02 10:32:58 -0500

    Commit #3

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Adding line - Commit #3
+ 
  */
 package ca.sheridancollege.week1.softwarefundamentals;
 
